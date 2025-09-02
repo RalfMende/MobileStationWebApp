7f3e521fd4f9ba837b22f389f52ed203e1c17758
Ralf_ <91413753+RalfMende@users.noreply.github.com>
2025-09-02 21:52:21 +0200

    Changed license to "beer-ware"

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+"""
+THE BEER-WARE LICENSE (Revision 42)
+
+<mende.r@hotmail.de> wrote this file. As long as you retain this notice you can do whatever you want with this
+stuff. If we meet someday, and you think this stuff is worth it, you can
+buy me a beer in return.
+Ralf Mende
+"""
+
 from flask import Flask, request, jsonify, render_template, Response, stream_with_context
 import socket
 import os
