1cadb278cf3559d7f5f92530af6fe7fc05a4f353
Ralf_ <91413753+RalfMende@users.noreply.github.com>
2025-09-14 20:55:35 +0200

    Keyboard now uses red and green from magicons_ by default

--- src/frontend/static/script.js
+++ src/frontend/static/script.js
@@ -1006,14 +1006,14 @@ function updateSwitchUI(btn1, btn2, valueNum) {
     // btn1 active, btn2 inactive
     btn1.classList.add('active');
     btn2.classList.remove('active');
-    if (img1) img1.src = asset('magicons_/switch_re_active.png');
-    if (img2) img2.src = asset('magicons_/switch_gr_inactive.png');
+    if (img1) img1.src = asset('magicons_/MagIcon_00_00_a.png');
+    if (img2) img2.src = asset('magicons_/MagIcon_00_01_i.png');
   } else {
     // btn1 inactive, btn2 active
     btn1.classList.remove('active');
     btn2.classList.add('active');
-    if (img1) img1.src = asset('magicons_/switch_re_inactive.png');
-    if (img2) img2.src = asset('magicons_/switch_gr_active.png');
+    if (img1) img1.src = asset('magicons_/MagIcon_00_00_i.png');
+    if (img2) img2.src = asset('magicons_/MagIcon_00_01_a.png');
   }
 }
 
